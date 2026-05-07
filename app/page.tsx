dac1b1dc6791326f155590e211ae15a227382bad
Yang <19787969@student.westernsydney.edu.au>
2026-05-07 18:44:01 +1000

    add bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1>My Awesome Blog</h1>
+      <h1>fsd hello</h1>
+      <p>My Awesome Blog</p>
       <p>Welcome to a CI/CD-ready blog app!</p>
     </main>
   );
